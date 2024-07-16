915fc8eb742274957efab9f181bcb2a67761c687
Charlie Egan <charlieegan3@users.noreply.github.com>
2024-07-16 20:45:43 +0100

    init

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "example-flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { nixpkgs, flake-utils, ... }:
+    let
+      utils = flake-utils;
+    in
+    utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        formatter = pkgs.nixpkgs-fmt;
+
+        devShell = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            hello
+          ];
+        };
+      }
+    );
+}
